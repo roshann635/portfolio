ed13e0b1b26c97afa62a0de72066bca4f32cad6e
roshann635 <roshanjadhav4385@gmail.com>
2026-03-28 17:02:52 +0530

    fix cors for production

--- server/server.js
+++ server/server.js
@@ -18,12 +18,23 @@ connectDB();
 // Middleware
 import cors from "cors";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://portfolio-weld-eight-88.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://portfolio-weld-eight-88.vercel.app",
-    ],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
